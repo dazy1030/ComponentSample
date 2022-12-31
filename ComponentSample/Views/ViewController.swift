e1e7a288c43b89401a0b2b5f308d8d54c98ae223
dazy1030 <odajima_naoki@cyberagent.co.jp>
2022-12-31 21:37:31 +0900

    プロジェクト初期化

---
+++ ComponentSample/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ComponentSample
+//
+//  Created by 小田島 直樹 on 2022/12/31.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
