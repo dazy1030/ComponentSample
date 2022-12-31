59c2d433ecc4abc87eb1eba79aa03920d244b1cb
dazy1030 <odajima_naoki@cyberagent.co.jp>
2023-01-01 01:38:02 +0900

    コンポーネントのリストを仮表示

--- ComponentSample/Views/RootViewController.swift
+++ ComponentSample/Views/RootViewController.swift
@@ -7,10 +7,57 @@
 
 import UIKit
 
+extension RootViewController {
+    enum Section {
+        case components
+    }
+    
+    enum Item: Identifiable, CaseIterable {
+        case sample
+        
+        var name: String {
+            switch self {
+            case .sample: return "Sample"
+            }
+        }
+        
+        var id: String {
+            name
+        }
+    }
+}
+
 final class RootViewController: UIViewController {
+    @IBOutlet private weak var componentCollectionView: UICollectionView! {
+        didSet {
+            let configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+            let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+            componentCollectionView.collectionViewLayout = layout
+            componentCollectionView.delegate = self
+        }
+    }
+    
+    typealias ListCellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Item>
+    private let cellRegistration = ListCellRegistration { cell, _, item in
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = item.name
+        cell.contentConfiguration = contentConfiguration
+        cell.accessories = [.disclosureIndicator()]
+    }
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Item>
+    private lazy var componentDataSource = DataSource(collectionView: componentCollectionView) { [unowned self] collectionView, indexPath, item in
+        collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.components])
+        snapshot.appendItems(Item.allCases)
+        componentDataSource.apply(snapshot)
     }
     
     private func setup() {
@@ -19,3 +66,10 @@ final class RootViewController: UIViewController {
     }
 }
 
+// MARK: - UICollectionViewDelegate
+
+extension RootViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+}
